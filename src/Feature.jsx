8bbfe910c60d91abbf998aabd5e1e55a477621ec
MarziaJalili <astronomyfancy@gmail.com>
2024-12-20 15:55:48 +0430

    fixed the error

--- src/Feature.jsx
+++ src/Feature.jsx
@@ -15,7 +15,8 @@ export default function Feature(props) {
         <section className="faq-accordion__feature">
             <div className="feature__question">
                 <h2>{props.question}</h2>
-                <button onClick={handleClick} className="feature__show-answer">
+                <button onClick={handleClick} className="feature__show-answer" aria-label="Toggle the answer">
+                    <i className="icon-toggle" aria-hidden="true" />
                     <img src={questionIcon} alt="" />
                 </button>
             </div>
